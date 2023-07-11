9f3caf9b31a17aef894d39cca6014db2ba3460e0
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-07-12 01:06:19 +0600

    app done

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView counttxt;
+    private Button countbtn;
+
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counttxt = findViewById(R.id.counttxt);
+        countbtn = findViewById(R.id.countbtn);
+
+        countbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counttxt.setText(""+ counting());
+            }
+        });
+
+    }
+    public int counting(){
+       return ++count;
     }
 }
